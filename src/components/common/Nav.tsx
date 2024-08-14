1104c83df38dc5c67245f7a69b4cc1e20c5c580b
seohyunCho <shanny06@naver.com>
2024-08-14 18:55:00 +0900

    Feat: nav바 생성

---
+++ src/components/common/Nav.tsx
@@ -0,0 +1,16 @@
+import styles from '@/styles/common/nav.module.css'
+
+const Nav = () => {
+    return <div className={styles.nav}>
+        <div className={styles.title_box}>
+            <div className={styles.baro_logo}>BARO</div>
+            <input placeholder="게시글을 검색하세요" className={styles.search_input}/>
+        </div>
+        <div className={styles.button_box}>
+            <div>글쓰기</div>
+            <div>마이페이지</div>
+        </div>
+    </div>
+}
+
+export default Nav;
