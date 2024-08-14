6d825d7a7dd33b4eb2e7f5cf1f30152fcb6f5b97
seohyunCho <shanny06@naver.com>
2024-08-14 19:00:15 +0900

    Feat: 사용자 정보 박스, 스콥바 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
-import styles from "@/styles/page.module.css";
+import Community from "@/components/main/community/Community";
+import UserInfoCard from "@/components/main/infoCard/UserInfoCard";
+import styles from '@/styles/main/main.module.css'
 
 export default function Home() {
-  return <div>Home</div>
+  return <main className={styles.main}>
+    <UserInfoCard />
+    <Community />
+  </main>
 }
